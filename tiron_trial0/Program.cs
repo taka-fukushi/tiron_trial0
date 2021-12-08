46f33a2d1d616ce3eb621f54dcb2d9e08f3979b2
mjcsoft@outlook.jp <mjcsoft@outlook.jp>
2021-12-08 11:28:50 +0900

    trial1

--- tiron_trial0/Program.cs
+++ tiron_trial0/Program.cs
@@ -16,7 +16,7 @@ namespace tiron_trial0
 		{
 			int i; // for test
 
-			i = 1234;
+			i = 12345;
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Tiron_trial_main());
